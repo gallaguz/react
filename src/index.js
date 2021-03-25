58c727d3754d38bd7a43c32be0aa31a3e38b238b
gallaguz <gallaguz@gmail.com>
2021-03-25 04:50:12 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import { Test } from "@components/app"
+import {Test} from "@components/app"
 import React from "react"
 import ReactDOM from "react-dom"
 // import styles from "./index.module.css"
@@ -8,21 +8,22 @@ import "./index.css"
 const messages = ["Hello..."]
 
 const Messages = () => {
-  return (
-    <div>
-      <h1>messages</h1>
-      {messages.map((message) => (
-        <p key={messages}>{message}</p>
-      ))}
-      <input placeholder="Введите сообщение" />
-      <button>Отправить</button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>messages</h1>
+            {messages.map((message) => (
+                <p key={messages}>{message}</p>
+            ))}
+            <input placeholder="Введите сообщение"/>
+            <button>Отправить</button>
+        </div>
+    )
 }
 
 ReactDOM.render(
-  <>
-    <Messages title="title" />
-  </>,
-  document.querySelector("#root"),
+    <>
+        <Messages title="title"/>
+        <Test/>
+    </>,
+    document.querySelector("#root"),
 )
